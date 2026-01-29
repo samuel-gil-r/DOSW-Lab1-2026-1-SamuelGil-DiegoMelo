5f84cc130c5416da36ca8861ee97c41815779002
Samuel Gil <samuel.gil-r@mail.escuelaing.edu.co>
2026-01-28 21:44:43 -0500

    Carril 2: lambda minimo y cantidad, limpieza de gitignore

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -1,4 +1,39 @@
-package CarreraParalela;
+package reto2;
 
-public class Reto2 {
+import java.util.List;
+import java.util.function.Function;
+
+public class CarreraParalela {
+
+    public static class Resultado {
+        public int minimo;
+        public int cantidad;
+    }
+
+
+    public static Resultado calcularMinimoYCantidad(List<Integer> numeros) {
+
+        Function<List<Integer>, Resultado> lambdaMinimoCantidad = lista -> {
+            Resultado r = new Resultado();
+
+            r.minimo = lista.stream()
+                    .min((a, b) -> a.compareTo(b))
+                    .orElse(0);
+
+            r.cantidad = lista.size();
+
+            return r;
+        };
+
+        return lambdaMinimoCantidad.apply(numeros);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> numeros = List.of(5, 8, 2, 9, 1);
+
+        Resultado resultado = calcularMinimoYCantidad(numeros);
+
+        System.out.println("Mínimo: " + resultado.minimo);
+        System.out.println("Cantidad: " + resultado.cantidad);
+    }
 }
