d1ec6b67edf4818a33d95cdceaa7c372f5369d47
Samuel Gil <samuel.gil-r@mail.escuelaing.edu.co>
2026-01-29 17:14:56 -0500

    Reto 3 StringBuffer reverse y eco especial

--- Laboratorio1/reto3/EcoMisterioso.java
+++ Laboratorio1/reto3/EcoMisterioso.java
@@ -5,23 +5,39 @@ import java.util.stream.IntStream;
 
 public class EcoMisterioso {
 
+
     public static String repetirTresBuilder(String mensaje) {
         return IntStream.range(0, 3)
                 .mapToObj(i -> mensaje)
                 .reduce(new StringBuilder(),
-                        (sb, s) -> sb.length() == 0 ? sb.append(s) : sb.append(" ").append(s),
+                        (sb, s) -> {
+                            if (sb.length() == 0) {
+                                sb.append(s);
+                            } else {
+                                sb.append(" ").append(s);
+                            }
+                            return sb;
+                        },
                         StringBuilder::append)
                 .toString();
     }
 
+
+
+    public static String invertirBuffer(String mensaje) {
+        return new StringBuffer(mensaje).reverse().toString();
+    }
+
+
     public static String ecoEspecial(String mensaje) {
         Function<String, String> repetir = EcoMisterioso::repetirTresBuilder;
         String repetido = repetir.apply(mensaje);
-        return new StringBuilder(repetido).reverse().toString();
+        return invertirBuffer(repetido);
     }
 
     public static void main(String[] args) {
         System.out.println(repetirTresBuilder("hola"));
+        System.out.println(invertirBuffer("hola"));
         System.out.println(ecoEspecial("hola"));
     }
 }
