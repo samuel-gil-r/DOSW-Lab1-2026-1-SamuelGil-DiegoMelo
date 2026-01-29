62b7309490e8778b73259eccf2d602ce15e34c92
Samuel Gil <samuel.gil-r@mail.escuelaing.edu.co>
2026-01-29 15:37:30 -0500

    Reto 2 TErmiando

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -1,47 +1,53 @@
 package reto2;
 
 import java.util.List;
-import java.util.function.Function;
 
 public class CarreraParalela {
 
     public static class Resultado {
         public int minimo;
+        public int maximo;
         public int cantidad;
+
+        public boolean mayorEsMultiploDe2;
+        public boolean mayorEsDivisorDe2;
+        public boolean cantidadEsPar;
+        public boolean cantidadEsImpar;
     }
 
+    public static Resultado combinarResultados(List<Integer> numeros) {
+        Resultado r = new Resultado();
 
-    public static Resultado calcularMinimoYCantidad(List<Integer> numeros) {
+        r.minimo = numeros.stream()
+                .min(Integer::compareTo)
+                .orElse(0);
 
-        Function<List<Integer>, Resultado> lambdaMinimoCantidad = lista -> {
-            Resultado r = new Resultado();
+        r.maximo = numeros.stream()
+                .max(Integer::compareTo)
+                .orElse(0);
 
-            r.minimo = lista.stream()
-                    .min((a, b) -> a.compareTo(b))
-                    .orElse(0);
+        r.cantidad = numeros.size();
 
-            r.cantidad = lista.size();
+        r.mayorEsMultiploDe2 = (r.maximo % 2 == 0);
+        r.mayorEsDivisorDe2 = (r.maximo != 0) ? (2 % r.maximo == 0) : false;
 
-            return r;
-        };
+        r.cantidadEsPar = (r.cantidad % 2 == 0);
+        r.cantidadEsImpar = (r.cantidad % 2 != 0);
 
-        return lambdaMinimoCantidad.apply(numeros);
+        return r;
     }
 
     public static void main(String[] args) {
         List<Integer> numeros = List.of(5, 8, 2, 9, 1);
 
-        Resultado resultado = calcularMinimoYCantidad(numeros);
+        Resultado r = combinarResultados(numeros);
 
-        System.out.println("Mínimo: " + resultado.minimo);
-        System.out.println("Cantidad: " + resultado.cantidad);
-    }
-
-
-    // Tu parte (Diego):
-    public Integer obtenerNumeroMasGrande(List<Integer> numeros) {
-        return numeros.stream()
-                .max((n1, n2) -> n1.compareTo(n2))
-                .orElse(0);
+        System.out.println("Minimo: " + r.minimo);
+        System.out.println("Maximo: " + r.maximo);
+        System.out.println("Cantidad: " + r.cantidad);
+        System.out.println("Mayor multiplo de 2: " + r.mayorEsMultiploDe2);
+        System.out.println("Mayor divisor de 2: " + r.mayorEsDivisorDe2);
+        System.out.println("Cantidad par: " + r.cantidadEsPar);
+        System.out.println("Cantidad impar: " + r.cantidadEsImpar);
     }
 }
