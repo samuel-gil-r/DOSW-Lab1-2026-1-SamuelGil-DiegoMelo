c07e81b63f1d7e2ae7c54be433d0b8cee7c77d46
juandiegomelo21-commits <juandiegomelo21@gmail.com>
2026-01-30 11:14:10 -0500

    Reto#2 culminado: Creación de CarreraParalela con lógica unificada

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -1,53 +1,27 @@
 package reto2;
 
 import java.util.List;
+import java.util.IntSummaryStatistics;
 
 public class CarreraParalela {
+    public static void procesarLista(List<Integer> numeros) {
+        IntSummaryStatistics stats = numeros.stream()
+                .mapToInt(Integer::intValue)
+                .summaryStatistics();
 
-    public static class Resultado {
-        public int minimo;
-        public int maximo;
-        public int cantidad;
+        // Lógica unificada (Samuel + Diego)
+        int max = stats.getMax();
+        int min = stats.getMin();
+        long count = stats.getCount();
 
-        public boolean mayorEsMultiploDe2;
-        public boolean mayorEsDivisorDe2;
-        public boolean cantidadEsPar;
-        public boolean cantidadEsImpar;
-    }
+        System.out.println("Maximo: " + max);
+        System.out.println("Minimo: " + min);
+        System.out.println("Cantidad: " + count);
 
-    public static Resultado combinarResultados(List<Integer> numeros) {
-        Resultado r = new Resultado();
-
-        r.minimo = numeros.stream()
-                .min(Integer::compareTo)
-                .orElse(0);
-
-        r.maximo = numeros.stream()
-                .max(Integer::compareTo)
-                .orElse(0);
-
-        r.cantidad = numeros.size();
-
-        r.mayorEsMultiploDe2 = (r.maximo % 2 == 0);
-        r.mayorEsDivisorDe2 = (r.maximo != 0) ? (2 % r.maximo == 0) : false;
-
-        r.cantidadEsPar = (r.cantidad % 2 == 0);
-        r.cantidadEsImpar = (r.cantidad % 2 != 0);
-
-        return r;
-    }
-
-    public static void main(String[] args) {
-        List<Integer> numeros = List.of(5, 8, 2, 9, 1);
-
-        Resultado r = combinarResultados(numeros);
-
-        System.out.println("Minimo: " + r.minimo);
-        System.out.println("Maximo: " + r.maximo);
-        System.out.println("Cantidad: " + r.cantidad);
-        System.out.println("Mayor multiplo de 2: " + r.mayorEsMultiploDe2);
-        System.out.println("Mayor divisor de 2: " + r.mayorEsDivisorDe2);
-        System.out.println("Cantidad par: " + r.cantidadEsPar);
-        System.out.println("Cantidad impar: " + r.cantidadEsImpar);
+        // Validaciones requeridas (Par/Impar y Múltiplos)
+        System.out.println("¿Max es múltiplo de 2?: " + ((max % 2 == 0) ? "Si" : "No"));
+        System.out.println("¿Max es divisor de 2?: " + ((max != 0 && 2 % max == 0) ? "Si" : "No"));
+        System.out.println("¿Cantidad es par?: " + ((count % 2 == 0) ? "Si" : "No"));
+        System.out.println("¿Cantidad es impar?: " + ((count % 2 != 0) ? "Si" : "No"));
     }
 }
